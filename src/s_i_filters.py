7b30eb1577d7ef8526561dfd5c3ce29520170066
Vania <zvulon.back@gmail.com>
2011-06-24 03:07:04 +0300

    stem implementation

--- src/s_i_filters.py
+++ src/s_i_filters.py
@@ -18,4 +18,50 @@ def test_word_filter():
         res = word_filter(the_text)
         assert res == ['week', 'ago', 'ordered', 'kindle', 'and', 'when', 'was', 'delivered', 'the', 'battery', 'was', 'critical', 'stage', 'and', 'would', 'not', 'power']
         for r in res: print r
-        print             
+        print
+
+''' ======================  Steam Implementation ======================='''
+
+            #1) SSES → SS
+            #2) IES → I
+            #3) SS → SS
+            #4) S → NULL
+            #5) (m>0) EED → EE (m is the number of letters before EED)
+            #6) ED → NULL
+            #7) ING → NULL
+            #8) AT → ATE
+            #9) BL → BLE
+            #10) IZ → IZE
+            #Important note: For each word,      
+gRules = [
+ ('sses', 'ss'),
+ ('ies', 'i'),
+ ('ss', 'ss'),
+ ('s', ''),
+ ('eed', 'ee'),
+ ('ed', ''),
+ ('ing', ''),
+ ('at', 'ate'),
+ ('bl', 'ble'),
+ ('iz', 'ize')
+ ]
+
+
+def stem_word(word):
+    if word == 'eed':
+        return word
+    
+    for rule in gRules:
+        suffix, exchange = rule
+        suf_len = len(suffix)
+        #skip if less than suffix
+        if len(word) < suf_len: continue
+        #change suffix if match
+        if word[-suf_len:] == suffix:
+            res = word[:-suf_len] + exchange
+            return res
+    return word
+        
+def suffix_stem(terms):
+    res = map(stem_word, terms)
+    return res 
